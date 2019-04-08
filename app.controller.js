302fdef253bab807ad4f09dea22929da9b91fbe6
HUMBERTO FLORES <hflores@nhaschools.com>
2019-04-08 15:27:52 -0400

    commit

--- app.controller.js
+++ app.controller.js
@@ -1,7 +1,40 @@
 "use strict";
-function MyCtrl($scope) {  
-  // Control the world
+function TodoController($scope) {  
+  const controller = this;
+  controller.todoList = [
+    {task: 'walk the dog', 
+  completed: false, 
+},
+{task:'Go to the store',
+completed: false 
+},
+{task: 'get gas',
+completed: false,
+},
+{task: 'wash the car',
+completed: false,
+},
+];
+
+
+controller.addTask = function(task) {
+  let newTask = {};
+  newTask.task = task;
+  newTask.completed = false;
+  controller.todoList.push(newTask);
 }
+
+controller.removeTask = function(removed) {    
+  controller.todoList.splice(removed, 1);
+}
+
+controller.completeTask = function(completed) {
+  controller.todoList[completed].completed = true;
+}
+
+
+}
+
 angular
-  .module("App")
-  .controller("MyCtrl", MyCtrl);
+.module("todoApp")
+.controller("TodoController", TodoController);
